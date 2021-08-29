a219f3c923157c32402160f054dfd9b7cd0be1ae
sprasad <sashi98@gmail.com>
2021-08-29 15:07:52 +0530

    requirement 2 | fare calculated based on minimum fare calculation policy

--- src/main/java/com/he/trainer/bootcamp/CabInvoice.java
+++ src/main/java/com/he/trainer/bootcamp/CabInvoice.java
@@ -4,8 +4,17 @@ public class CabInvoice {
 
     public static final int DIST_CHARGE_PER_KM = 10;
     public static final int WAITING_CHARGE_PER_MIN = 2;
+    public static final int MINIMUM_FARE = 40;
 
     public int calculateFare(int distance, int waitingTimeInMin) {
+        int fare = calculate(distance, waitingTimeInMin);
+        if (fare < 40) {
+            fare = MINIMUM_FARE;
+        }
+        return fare;
+    }
+
+    private int calculate(int distance, int waitingTimeInMin) {
         return (DIST_CHARGE_PER_KM * distance) + (WAITING_CHARGE_PER_MIN * waitingTimeInMin);
     }
 }
--- src/test/java/com/he/trainer/bootcamp/CabInvoiceTest.java
+++ src/test/java/com/he/trainer/bootcamp/CabInvoiceTest.java
@@ -2,10 +2,12 @@ package com.he.trainer.bootcamp;
 
 import com.he.trainer.bootcamp.CabInvoice;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class CabInvoiceTest {
 
+    @Disabled("This test can be ignored as fare calculation rule is changed and minimum 40rs fare policy applied.")
     @Test
     public void fare_should_be_10rs_for_1km_and_no_waiting_charges(){
         CabInvoice invoice = new CabInvoice();
@@ -13,6 +15,7 @@ public class CabInvoiceTest {
         Assertions.assertEquals(10, fare);
     }
 
+    @Disabled("This test can be ignored as fare calculation rule is changed and minimum 40rs fare policy applied.")
     @Test
     public void fare_should_be_12rs_for_1km_and_1min_waiting_charges(){
         CabInvoice invoice = new CabInvoice();
@@ -20,10 +23,25 @@ public class CabInvoiceTest {
         Assertions.assertEquals(12, fare);
     }
 
+    @Disabled("This test can be ignored as fare calculation rule is changed and minimum 40rs fare policy applied.")
     @Test
     public void fare_should_be_24rs_for_2km_and_2min_waiting_charges(){
         CabInvoice invoice = new CabInvoice();
         int fare = invoice.calculateFare(2, 2);
         Assertions.assertEquals(24, fare);
+    }
+
+    @Test
+    public void fare_should_be_minimum_40rs(){
+        CabInvoice invoice = new CabInvoice();
+        int fare = invoice.calculateFare(2, 2);
+        Assertions.assertEquals(40, fare);
+    }
+
+    @Test
+    public void fare_should_be_50rs_for_4km_and_5min_waiting(){
+        CabInvoice invoice = new CabInvoice();
+        int fare = invoice.calculateFare(4, 5);
+        Assertions.assertEquals(50, fare);
     }
 }
